8efeacd902108e34d122ac6b6d56a5602bab3741
Panda <linimbus@126.com>
2017-09-13 18:05:22 +0800

    Create test_server.go

---
+++ test_server.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"golang_demo/srpc/srpc"
+	//"os"
+	//"runtime/pprof"
+	"time"
+)
+
+type SAVE struct {
+	tmp uint32
+}
+
+func (s *SAVE) Add(a uint32, b *uint32) error {
+
+	*b = a + 1
+
+	//fmt.Println("call add ", a, *b, s.tmp)
+
+	return nil
+}
+
+func (s *SAVE) Sub(a uint32, b *uint32) error {
+
+	*b = a - 1
+
+	//fmt.Println("call sub ", a, *b, s.tmp)
+
+	return nil
+}
+
+func Server(addr string) {
+
+	var s SAVE
+
+	s.tmp = 100
+
+	//f, _ := os.Create("profile_file")
+	//pprof.StartCPUProfile(f)     // 开始cpu profile，结果写到文件f中
+	//defer pprof.StopCPUProfile() // 结束profile
+
+	server := srpc.NewServer(addr)
+	server.BindMethod(&s)
+
+	err := server.Start()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	for i := 0; i < 100; i++ {
+		time.Sleep(time.Second)
+	}
+
+	server.Stop()
+}
+
+func main() {
+	Server(":1234")
+}
