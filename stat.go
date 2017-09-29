863059656c30cd9021a7505296d03c6d2a1253e6
Panda <linimbus@126.com>
2017-09-29 22:49:19 +0800

    add stat code

---
+++ stat.go
@@ -0,0 +1,20 @@
+package srpc
+
+type Stat struct {
+	RecvCnt int64
+	SendCnt int64
+	ErrCnt  int64
+}
+
+func (s1 Stat) Sub(s2 Stat) Stat {
+	s1.RecvCnt -= s2.RecvCnt
+	s1.SendCnt -= s2.SendCnt
+	s1.ErrCnt -= s2.ErrCnt
+	return s1
+}
+
+var stat Stat
+
+func GetStat() Stat {
+	return stat
+}
