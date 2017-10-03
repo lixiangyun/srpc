0f856058937c2ea9dbaa4705833089fb9582c299
Panda <linimbus@126.com>
2017-10-03 22:00:53 +0800

    add benchmark demo

---
+++ benchmark/server/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"log"
+	"os"
+	"runtime"
+	"srpc"
+	"time"
+)
+
+var server *srpc.Server
+
+type SAVE struct {
+	tmp uint32
+}
+
+func (s *SAVE) Add(a uint32, b *uint32) error {
+	*b = a + 1
+	//log.Println("call add ", a, *b, s.tmp)
+	return nil
+}
+
+func (s *SAVE) Sub(a uint32, b *uint32) error {
+	*b = a - 1
+	//log.Println("call sub ", a, *b, s.tmp)
+	return nil
+}
+
+func netstat() {
+
+	s1 := srpc.GetStat()
+	log.Println("start stat...")
+
+	for {
+
+		time.Sleep(time.Second)
+		s2 := srpc.GetStat()
+		s3 := s2.Sub(s1)
+		s1 = s2
+
+		log.Printf("Throughput %.3f kTPS \r\n", float32(s3.SendCnt)/(1024))
+	}
+}
+
+func Server(addr string) {
+
+	var s SAVE
+
+	s.tmp = 100
+
+	cpunum := runtime.NumCPU()
+	runtime.GOMAXPROCS(cpunum)
+
+	log.Println("max cpu num: ", cpunum)
+
+	server = srpc.NewServer(addr)
+	server.RegMethod(&s)
+
+	go server.Start()
+
+	netstat()
+}
+
+func main() {
+	addr := ":1234"
+	args := os.Args
+	if len(args) == 2 {
+		addr = args[1]
+	}
+	log.Println("Addr ", addr)
+	Server(addr)
+}
