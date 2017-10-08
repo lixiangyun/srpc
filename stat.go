e0da20225001dc7ef9113ebfa5f77f348ede67f8
Panda <linimbus@126.com>
2017-10-08 20:31:12 +0800

    add comment

--- stat.go
+++ stat.go
@@ -1,11 +1,13 @@
 package srpc
 
+// 通过信息结构
 type Stat struct {
-	RecvCnt int64
-	SendCnt int64
-	ErrCnt  int64
+	RecvCnt int64 // 请求接收次数的统计
+	SendCnt int64 // 请求发送次数的统计
+	ErrCnt  int64 // 出现错误次数的统计
 }
 
+// 两个统计差值，s1=s1-s2，并且返回s1结果
 func (s1 Stat) Sub(s2 Stat) Stat {
 	s1.RecvCnt -= s2.RecvCnt
 	s1.SendCnt -= s2.SendCnt
@@ -13,12 +15,15 @@ func (s1 Stat) Sub(s2 Stat) Stat {
 	return s1
 }
 
+// 全局统计的统计
 var stat Stat
 
+// 获取当前全局的统计信息
 func GetStat() Stat {
 	return stat
 }
 
+// 累计统计的信息
 func StatAdd(recv, send, err int64) {
 	stat.RecvCnt += recv
 	stat.SendCnt += send
